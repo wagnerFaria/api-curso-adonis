17d2313ec68ac43a15bf572440666e6aaac078a3
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2020-05-15 14:19:09 -0400

    Adicionando validadores para resource

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -8,6 +8,17 @@ class UserController {
     async show(){
         return {result: 'buscou o usuario 1'}
     }
+    async update(){
+        return {result: 'atualizando o usuario 1'}
+    }
+
+    async store(){
+        return {result: 'buscou o usuario 1'}
+    }
+
+    async destroy(){
+        return {result: 'buscou o usuario 1'}
+    }
 }
 
 module.exports = UserController
---
+++ app/Validators/User.js
@@ -0,0 +1,26 @@
+'use strict'
+
+class User {
+  get rules() {
+    return {
+      name: 'required',
+      username: 'required',
+      email: 'required',
+      password: 'required',
+      type_user_id: 'required',
+    }
+  }
+
+
+  get messages() {
+    return {
+      'name.required': 'Informe a propriedade name',
+      'username.required': 'Informe a propriedade username',
+      'email.required': 'Informe a propriedade email',
+      'password.required': 'Informe a propriedade password',
+      'type_user_id.required': 'Informe a propriedade type_user_id',
+    }
+  }
+}
+
+module.exports = User
